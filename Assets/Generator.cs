3eed2bbd090bdfa6123f110b95c1997f7b747b0a
DESKTOP-JKDI5O7\linke <linkenzone@163.com>
2020-09-22 11:01:05 +0800

    柏林噪声的demo

---
+++ Assets/Generator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    //污预置件
+    public GameObject dirtPerfab;
+    public GameObject caoPerfab;
+
+
+    int minX = -32;
+    int maxX = 32;
+    int minY = -10;
+    int maxY = 10;
+
+    PerlinNoise noise;
+
+    void Start()
+    {
+        noise = new PerlinNoise(Random.Range(100000,10000000));
+        Regenerate();
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void Regenerate()
+    {
+        float width = dirtPerfab.transform.lossyScale.x;
+        float height = dirtPerfab.transform.lossyScale.y;
+
+
+        for (int i = minX; i < maxX ; i++) //x值
+        {
+            int columnHeight = 2 + noise.getNoise(i - minX,maxY - minY -2);
+            for (int j = minY; j < minY + columnHeight; j++) //y值
+            {
+                GameObject block = (j == minY + columnHeight - 1) ? caoPerfab : dirtPerfab;
+                Instantiate(block, new Vector2(i * width , j * height) , Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/PerlinNoise.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinNoise
+{
+    long seed; //种子
+
+    public PerlinNoise(long seed)
+    {
+        this.seed = seed;
+    }
+
+    //产生伪随机数
+    private int random(int x,int range)
+    {
+        return (int)((x + seed) ^ 5) % range;
+    }
+
+    public int getNoise(int x,int range)
+    {
+        int chunkSize = 16; //振动频率，波长
+        float noise = 0;
+        range /= 2; //range/2 + range/4 + range/8+....
+        while (chunkSize > 0)
+        {
+            int chunkIndex = x / chunkSize;
+
+            float prog = (x % chunkSize) / (chunkSize * 1f);
+
+            float left_random = random(chunkIndex, range);
+            float right_random = random(chunkIndex+1, range);
+
+            //生成噪点
+            noise += (1 - prog) * left_random + prog * right_random;
+
+        chunkSize /= 2;
+        range /= 2;
+
+        range = Mathf.Max(1, range);
+    }
+        //四舍五入
+        return (int)Mathf.Round(noise);
+    }
+
+}
